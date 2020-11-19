4eeeb8bf5272f58e5c3418c05688e09060da17a0
Hung X. Tran <68488508+hungxtran@users.noreply.github.com>
2020-11-19 11:49:51 +0700

    Added Open email by 2 methods

--- EmailAndLauncherBug/MainPage.xaml.cs
+++ EmailAndLauncherBug/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace EmailAndLauncherBug
 {
@@ -13,6 +14,38 @@ namespace EmailAndLauncherBug
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void Email_1_Tapped(Object sender, EventArgs e)
+        {
+            try
+            {
+                Email.ComposeAsync(null, null, "rrt@we-worldwide.com");
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                Console.WriteLine("Email not supported on this device. Exception: " + ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Other exception: " + ex);
+            }
+        }
+
+        void Email_2_Tapped(Object sender, EventArgs e)
+        {
+            try
+            {
+                Launcher.OpenAsync("mailto:xboxpress@assemblyinc.com");
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                Console.WriteLine("Launcher not supported on this device. Exception: " + ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Other exception: " + ex);
+            }
         }
     }
 }
